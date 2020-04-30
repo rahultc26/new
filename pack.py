7fe5e402653d0c47b091d64fc646d2bbcd31e6a1
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2020-04-30 12:03:16 +0530

    using pack method in tkinter

---
+++ pack.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+ttk.Label(text='Hello, Rahul',background='yellow').pack(side=LEFT)
+ttk.Label(text='Hello, rahul',background='skyblue').pack(side=LEFT)
+ttk.Label(text='Hello, rahul',background='green').pack(side=LEFT)
+
+root.mainloop()
